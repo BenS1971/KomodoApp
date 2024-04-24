d271ae13607348aaa2e9c71d25e0aa7dd17dc807
Ben Strycker <bgstrycker1971@gmail.com>
2024-04-24 19:38:59 -0400

    Working on Dev team repo...

--- Dev_Team_Repository/DevTeamRepo.cs
+++ Dev_Team_Repository/DevTeamRepo.cs
@@ -3,5 +3,64 @@ using Dev_Team;
 //Here I will drop the POCOs (No, not the 1970's music group!) for the Dev_Teams.
 public class Dev_Teams
 {
-    
+    private List<Dev_Teams>  _listOfContent = new List<Dev_Teams>();
+
+    //Okay, here we go with my CRUD methods in this repository. Let's start with CREATE...
+    public void AddContentToList(Dev_Teams content)
+    {
+         _listOfContent.Add(content);    
+    }
+    // Now on to the READ method...
+
+    public List<Dev_Teams> GetContentList()
+    {
+        return new List<Dev_Teams>(_listOfContent);
+    }
+
+    //Now the UPDATE method...
+    public bool UpdateDev_Teams(Dev_Teams content)
+    {
+        //Find the Dev Teams to update...
+        Dev_Teams oldContent = GetContentList(Dev_Teams);
+        //Update Dev Teams...
+        if (oldContent != oldContent)
+        {   
+            oldContent.Dev_Team_ID = content.Dev_Team_ID;
+            oldContent.Dev_Team_Member_Name = content.dev_team_member_name;
+            oldContent.Dev_Team_Name = content.dev_team_name;
+           
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    // Now we start deleting stuff here!
+    public bool DeleteDev_Teams(string lastname)
+    {
+        Dev_Teams content = GetDev_Dev_Teams(lastname);    
+
+        if (content == null)
+        {
+            return false;
+        } 
+        
+        if (InitialCount > _listOfContent.Count)
+        {
+            return true;
+        }
+    }
+    // Helper method goes here. (And NOT TUNA or HAMBURGER Helper, either!!)
+    public Dev_Teams GetDev_Dev_Teams(string lastName)
+    {
+        foreach (Dev_Teams in_listOfContent)
+        {
+            if (Dev_Teams.lastName.ToLower() == lastName.ToLower())
+            {
+                return content;
+            }
+        }
+        return null;
+    }
 }
